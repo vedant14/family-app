b7ba74ece0ae63a5ce2a5df3ef169bc36a5cd58b
vedant14 <vedant.lohbare@gmail.com>
2025-03-31 16:23:23 +0530

    Fixed a small icon that I missed

--- app/components/ui/icons.jsx
+++ app/components/ui/icons.jsx
@@ -11,6 +11,19 @@ export const IconCirclePlusFilled = (props) => (
     <path d="M4.929 4.929a10 10 0 1 1 14.141 14.141a10 10 0 0 1 -14.14 -14.14zm8.071 4.071a1 1 0 1 0 -2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0 -2h-2v-2z" />
   </svg>
 );
+export const IconFolderFilled = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    class="icon icon-tabler icons-tabler-filled icon-tabler-folder"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 3a1 1 0 0 1 .608 .206l.1 .087l2.706 2.707h6.586a3 3 0 0 1 2.995 2.824l.005 .176v8a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-11a3 3 0 0 1 2.824 -2.995l.176 -.005h4z" />
+  </svg>
+);
 
 export const IconDotsVertical = (props) => (
   <svg
