9a6b7e6c285f4bf42b5738fffc2c2898de82de7e
vedant14 <vedant.lohbare@gmail.com>
2025-04-26 15:20:48 +0530

    Better data points

--- app/components/ui/icons.jsx
+++ app/components/ui/icons.jsx
@@ -102,7 +102,6 @@ export const IconFilterCheck = (props) => (
 );
 export const IconReceipt = (props) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
@@ -111,7 +110,7 @@ export const IconReceipt = (props) => (
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="icon icon-tabler icons-tabler-outline icon-tabler-receipt"
+    {...props}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16l-3 -2l-2 2l-2 -2l-2 2l-2 -2l-3 2m4 -14h6m-6 4h6m-2 4h2" />
@@ -240,11 +239,10 @@ export const IconTrendingDown = (props) => (
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="icon icon-tabler icons-tabler-outline icon-tabler-trending-down"
+    {...props}
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M3 7l6 6l4 -4l8 8" />
--- app/routes/dashboard/dashboard.jsx
+++ app/routes/dashboard/dashboard.jsx
@@ -8,7 +8,11 @@ import {
   subMonthsUTC,
 } from "~/utils/dateHelpers";
 import { Link } from "react-router";
-import { IconTrendingDown, IconTrendingUp } from "~/components/ui/icons";
+import {
+  IconReceipt,
+  IconTrendingDown,
+  IconTrendingUp,
+} from "~/components/ui/icons";
 import { Badge } from "~/components/ui/badge";
 import {
   Card,
@@ -236,7 +240,7 @@ export async function loader({ params }) {
   const calculateNetAmount = (incomeData, expenseData) => {
     const income = incomeData?._sum?.amountExtract || 0;
     const expense = expenseData?._sum?.amountExtract || 0;
-    return income - expense;
+    return { income, expense };
   };
 
   const mergeCategoryGroups = (expenses, incomes) => {
@@ -384,64 +388,40 @@ export default function Dashboard({ loaderData }) {
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 mb-4 *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs">
       <div className="grid grid-cols-2 gap-4">
-        <Card className="@container/card h-fit">
-          <CardHeader>
-            <CardDescription>Spends this week</CardDescription>
-            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-              {formatIndianCurrency(transactions.thisWeek.amount)}
-            </CardTitle>
-            <CardAction>
-              <Badge variant="outline">
-                <ShowArrow
-                  oldValue={transactions.lastWeek.amount}
-                  newValue={transactions.thisWeek.amount}
-                  showValue={true}
-                />
-              </Badge>
-            </CardAction>
-          </CardHeader>
-          <CardFooter className="flex-col items-start gap-1.5 text-sm">
-            <div className="line-clamp-1 flex gap-2 font-medium">
-              Compared to last week
-              <ShowArrow
-                oldValue={transactions.lastWeek.amount}
-                newValue={transactions.thisWeek.amount}
-              />
-            </div>
-            <div className="text-muted-foreground">
-              {formatIndianCurrency(transactions.lastWeek.amount)}
-            </div>
-          </CardFooter>
-        </Card>
-        <Card className="@container/card h-fit">
-          <CardHeader>
-            <CardDescription>Spends this month</CardDescription>
-            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-              {formatIndianCurrency(transactions.thisMonth.amount)}
-            </CardTitle>
-            <CardAction>
-              <Badge variant="outline">
-                <ShowArrow
-                  oldValue={transactions.lastMonth.amount}
-                  newValue={transactions.thisMonth.amount}
-                  showValue={true}
-                />
-              </Badge>
-            </CardAction>
-          </CardHeader>
-          <CardFooter className="flex-col items-start gap-1.5 text-sm">
-            <div className="line-clamp-1 flex gap-2 font-medium">
-              Compared to last month
-              <ShowArrow
-                oldValue={transactions.lastMonth.amount}
-                newValue={transactions.thisMonth.amount}
-              />
-            </div>
-            <div className="text-muted-foreground">
-              {formatIndianCurrency(transactions.lastMonth.amount)}
-            </div>
-          </CardFooter>
-        </Card>
+        <DashboardCard
+          title="Cashflow"
+          values={[
+            {
+              value: transactions.thisMonth.amount.expense,
+              label: "OUTGOINGS",
+              shadowValue: transactions.lastMonth.amount.expense,
+            },
+            {
+              value: transactions.thisMonth.amount.income,
+              label: "INCOMINGS",
+              shadowValue: transactions.lastMonth.amount.income,
+              reverseColors: true,
+            },
+          ]}
+          showMonth={true}
+        />
+        <DashboardCard
+          title="This Week"
+          values={[
+            {
+              value: transactions.thisWeek.amount.expense,
+              label: "OUTGOINGS",
+              shadowValue: transactions.lastWeek.amount.expense,
+            },
+            {
+              value: transactions.thisWeek.amount.income,
+              label: "INCOMINGS",
+              shadowValue: transactions.thisWeek.amount.income,
+              reverseColors: true,
+            },
+          ]}
+          showMonth={false}
+        />
         <Card className="@container/card h-fit">
           <CardHeader>
             <CardDescription>Investments this month</CardDescription>
@@ -580,21 +560,7 @@ export default function Dashboard({ loaderData }) {
   );
 }
 
-const ShowArrow = ({
-  newValue,
-  oldValue,
-  showValue = false,
-  reverseColors = false,
-}) => {
-  const percentageChange =
-    newValue !== 0 && showValue
-      ? Math.round(((newValue - oldValue) / Math.abs(newValue)) * 100)
-      : null;
-
-  if (newValue === oldValue) {
-    return showValue ? <span>0 %</span> : null;
-  }
-
+const ShowArrow = ({ newValue, oldValue, reverseColors = false }) => {
   const isDown = oldValue > newValue;
   const downArrowColor = reverseColors ? "stroke-red-400" : "stroke-green-600";
   const upArrowColor = reverseColors ? "stroke-green-600" : "stroke-red-400";
@@ -602,13 +568,61 @@ const ShowArrow = ({
   return (
     <>
       {isDown ? (
-        <IconTrendingDown className={`size-5 ${downArrowColor}`} />
+        <IconTrendingDown className={`size-4 ${downArrowColor}`} />
       ) : (
-        <IconTrendingUp className={`size-5 ${upArrowColor}`} />
-      )}
-      {percentageChange !== null && (
-        <span className="ml-1">{percentageChange} %</span>
+        <IconTrendingUp className={`size-4 ${upArrowColor}`} />
       )}
     </>
   );
 };
+
+function DashboardCard({ title, values, showMonth }) {
+  return (
+    <Card className="@container/card">
+      <CardHeader>
+        <CardDescription className="flex justify-between items-center">
+          <div className="flex gap-x-1">
+            <IconReceipt className="h-5 text-gray-500" />
+            <span>{title}</span>
+          </div>
+          {/* <div className="w-24">
+            <select className="w-full border border-gray-200 text-gray-700 py-1.5 px-3 rounded-md leading-tight focus:outline-none text-xs appearance-none">
+              <option>May 2025</option>
+            </select>
+          </div> */}
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <div className="w-full">
+          {values.map((value, index) => (
+            <div
+              className="group flex justify-between items-center mb-4"
+              key={index}
+            >
+              <div className="flex items-center space-x-1">
+                <span className="text-neutral-700 font-semibold whitespace-nowrap text-2xl">
+                  {formatIndianCurrency(value.value)}
+                </span>
+                <span
+                  className="text-neutral-400 whitespace-nowrap overflow-hidden 
+                   opacity-0 max-w-0 group-hover:opacity-100 group-hover:max-w-md group-hover:ml-2 text-sm
+                   transition-all duration-400 ease-in-out"
+                >
+                  {formatIndianCurrency(value.shadowValue)}
+                </span>
+                <div className="transition-all duration-400 ease-in-out">
+                  <ShowArrow
+                    oldValue={value.shadowValue}
+                    newValue={value.value}
+                    reverseColors={value.reverseColors}
+                  />
+                </div>
+              </div>
+              <span className="text-xs text-gray-500">{value.label}</span>
+            </div>
+          ))}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
